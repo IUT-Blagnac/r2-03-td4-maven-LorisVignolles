f3f2bd77774ceff5a207b02702db4bbfc3ee02ef
Loris Vignolles <126151481+LorisVignolles@users.noreply.github.com>
2023-02-24 14:51:22 +0100

    TP4 Qualité

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * TP1 EX2
+ *
+ * @author (Loris Vignolles)
+ */
+public class HelloWorld
+{
+    public static void main(String args[]) {
+        System.out.println("Hello Monde !");
+        System.out.println("depuis Blagnac.");
+        System.out.println("Ça va bien ?");
+    }
+}
+
